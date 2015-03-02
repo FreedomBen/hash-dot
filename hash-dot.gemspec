8b157322b7436cdcdd57c0fbae372862dd7522a0
Benjamin Porter <bporter@instructure.com>
2015-03-02 16:44:12 -0700

    Define method for faster access the second time
    Also add support for the = method, so you can set values through
    my_hash.key = val

--- hash-dot.gemspec
+++ hash-dot.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'hash-dot'
-  s.version     = '0.0.1'
-  s.date        = '2015-02-20'
+  s.version     = '0.0.2'
+  s.date        = '2015-03-02'
   s.summary     = "Adds the JavaScript hash syntax to ruby"
   s.description = "Adds the JavaScript dot notation for accessing hash keys to ruby, so you can do my_hash.key1"
   s.authors     = ["Ben Porter"]
--- lib/hash-dot.rb
+++ lib/hash-dot.rb
@@ -1,12 +1,32 @@
 class Hash
+  def define_reader(key)
+    define_method(key) do
+      if has_key?(key)
+        return self[key]
+      else
+        return self[key.to_sym]
+      end
+    end
+  end
+
+  def define_writer(key)
+    define_method(key) do |value|
+      self[key[0..-2].to_sym] = value
+    end
+  end
+
   def method_missing(method, *opts)
     m = method.to_s
     msym = m.to_sym
-    if self.has_key?(m)
-      return self[m]
-    elsif self.has_key?(msym)
-      return self[msym]
+
+    if m[-1] == '='
+      define_writer(m)
+      return self.send(method, opts)
+    elsif has_key?(m) || has_key?(msym)
+      define_reader(m)
+      return self.send(method, opts)
+    else
+      super
     end
-    super
   end
 end
